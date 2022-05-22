1367b25e35d197d5f5c33ca2427ff6465c19ea83
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-05-22 21:58:53 +0700

    Progress Bar Change Profile

--- app/src/main/java/com/nandaiqbalh/tugaspbb/activity/userprofile/ChangeProfileActivity.java
+++ app/src/main/java/com/nandaiqbalh/tugaspbb/activity/userprofile/ChangeProfileActivity.java
@@ -12,6 +12,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -51,6 +52,8 @@ public class ChangeProfileActivity extends AppCompatActivity {
     UserProfileRequest userProfileRequest;
 
     User user, userUpdated;
+
+    ProgressBar pbChangeProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,6 +110,9 @@ public class ChangeProfileActivity extends AppCompatActivity {
         userUpdated = sharedPrefs.getUserUpdated();
 
         userProfileRequest = new UserProfileRequest();
+
+        pbChangeProfile = (ProgressBar) findViewById(R.id.pb_change_profile);
+
     }
 
     private void aturValueForm(){
@@ -253,11 +259,15 @@ public class ChangeProfileActivity extends AppCompatActivity {
             return;
         }
 
+        pbChangeProfile.setVisibility(View.VISIBLE);
+
         Call<UserProfileResponse> userUpdateProfileCall = ApiConfig.getService().updateUserProfile(userProfileRequest);
         userUpdateProfileCall.enqueue(new Callback<UserProfileResponse>() {
 
             @Override
             public void onResponse(Call<UserProfileResponse> call, Response<UserProfileResponse> response) {
+
+                pbChangeProfile.setVisibility(View.GONE);
 
                 if (response.isSuccessful()) {
 
@@ -282,6 +292,8 @@ public class ChangeProfileActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<UserProfileResponse> call, Throwable t) {
+
+                pbChangeProfile.setVisibility(View.GONE);
 
                 String message = t.getLocalizedMessage();
                 Toast.makeText(ChangeProfileActivity.this, message, Toast.LENGTH_LONG).show();
