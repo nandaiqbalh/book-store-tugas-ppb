cd9a73c1ab5458d601395a99dacf98cbccdcff66
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-05-26 11:56:30 +0700

    Layouting Checkout form

--- app/src/main/java/com/nandaiqbalh/tugaspbb/activity/checkout/CheckoutActivity.java
+++ app/src/main/java/com/nandaiqbalh/tugaspbb/activity/checkout/CheckoutActivity.java
@@ -3,6 +3,7 @@ package com.nandaiqbalh.tugaspbb.activity.checkout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.nandaiqbalh.tugaspbb.R;
 
@@ -12,5 +13,9 @@ public class CheckoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        // full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
     }
 }
